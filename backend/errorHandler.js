4bf7db99deed63abbff8cc017a967c38f7923109
cameylon <62754572+cameylon@users.noreply.github.com>
2025-02-25 01:22:31 +1030

    Update errorHandler.js

--- backend/errorHandler.js
+++ backend/errorHandler.js
@@ -1,1 +1,5 @@
-
+// backend/middleware/errorHandler.js
+module.exports = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "Internal Server Error" });
+};
