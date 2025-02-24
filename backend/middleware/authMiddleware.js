2bfef50f4cf708073e4b7174d19617613648ac66
cameylon <62754572+cameylon@users.noreply.github.com>
2025-02-25 02:06:06 +1030

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,1 +1,9 @@
+// backend/middleware/authMiddleware.js
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    next();
+};
 
