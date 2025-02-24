6608312555d322e071da11ff44d697f4afbd9234
cameylon <62754572+cameylon@users.noreply.github.com>
2025-02-25 02:04:24 +1030

    Delete backend/errorHandler.js

--- backend/errorHandler.js
+++
@@ -1,5 +0,0 @@
-// backend/middleware/errorHandler.js
-module.exports = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: "Internal Server Error" });
-};
